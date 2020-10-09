0e969f381ff8854792d6a7fd3521c159f1e0b691
Guo, Jingfeng <jingfeng.guo@intel.com>
2020-10-09 13:36:10 +0800

    first commit

---
+++ struct.go
@@ -0,0 +1,45 @@
+package route
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Node struct {
+	Key int64
+	MMap []int64
+	MValue []float64
+}
+
+type UG struct {
+	Nodes map[int]*Node
+}
+
+func NewUG() *UG {
+	return &UG{
+		Nodes: make(map[int]*Node),
+	}
+}
+
+func (ug *UG) AddNode(n *Node) error {
+	if _, ok := ug.Nodes[n.Key]; ok {
+		return errors.New("Can not add a exited node")
+	}
+	ug.Nodes[n.Key] = n
+	return nil
+}
+
+func (ug *UG) AddNodes(n int64) {
+	for i := 0; i < n; i++ {
+		ug.Nodes[i] = &Node{Key: i}
+	}
+}
+
+func (ug *UG) AddMap(from int64, tos []int64, toValues []float64) {
+	for _, to := range tos {
+		ug.Nodes[from].MMap = append(ug.Nodes[from].MMap, to)
+	}
+	for _, toValue := range toValues {
+		ug.Nodes[from].MValue = append(ug.Nodes[from].MValue, toValue)
+	}
+}
---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+
+import {
+	"fmt"
+	"route"
+	"errors"
+}
+
+func main() {
+	ug := route.NewUG()
+	ug.AddNodes(5)
+}
