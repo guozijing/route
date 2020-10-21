8f010ed343195c424d74cf12eecbdcf0f97e71a7
Guo, Jingfeng <jingfeng.guo@intel.com>
2020-10-21 13:06:30 +0800

    add read graph from json file

--- struct.go
+++ struct.go
@@ -2,6 +2,9 @@ package simpleRoute
 
 import (
 	"errors"
+	"os"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type Node struct {
@@ -11,6 +14,15 @@ type Node struct {
 
 type UG struct {
 	Nodes map[int]*Node
+}
+
+type StructGraph struct {
+	Graph []*StructMap `json:"graph"`
+}
+
+type StructMap struct {
+	Index int `json:"index"`
+	LinkTo []int `json:"linkTo"`
 }
 
 func NewUG() *UG {
@@ -42,6 +54,29 @@ func (ug *UG) AddMap(from int, tos []int) error {
 			return errors.New("The node should be exited")
 		}
 		ug.Nodes[from].MMap = append(ug.Nodes[from].MMap, to)
+	}
+	return nil
+}
+
+func (ug *UG) AddMapFromFile(fileName string) error {
+	var content []byte
+	var err error
+	content, err = file_get_contents(path)
+	if err != nil {
+		return err
+	}
+
+	var c StructGraph
+	err = json.Unmarshal([]byte(content), &c)
+	if err != nil {
+		return err
+	}
+
+	for _, fromTos := range c {
+		err := AddMap(fromTos.Index, fromTos.LinkTo)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -82,4 +117,12 @@ func (ug *UG) GetRoutes(from int, to int) (map[int][]int, error) {
 	}
 	dfs([]int{}, from, isVisited)
 	return resM, nil
+}
+
+func file_get_contents(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(f)
 }
